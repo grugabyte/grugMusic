f034ce9cd791086cc3df037289286c2028b2e4e7
grugabyte <98688177+grugabyte@users.noreply.github.com>
2022-06-13 20:25:18 +1000

    Add files via upload

--- playlist01.sh
+++ playlist01.sh
@@ -4,11 +4,17 @@ echo "Starting playlist"
 
 #requires dependancies: youtube-dl and mpv
 
+echo "Now Playing: Swedish House Mafia - Don't you worry child"
 mpv --no-video "https://www.youtube.com/watch?v=3mWbRB3Y4R8"	# Swedish House Mafia - Don't you worry child
+echo "Now Playing: Macklemore - Downtown"
 mpv --no-video "https://www.youtube.com/watch?v=l-uVkrzo8eU"	# Macklemore - Downtown
+echo "Now Playing: Bruno Mars - The Other Side"
 mpv --no-video "https://www.youtube.com/watch?v=DVCQ6m6V0YE"	# Bruno Mars - The Other Side
+echo "Now Playing: Maroon 5 - Sunday Morning"
 mpv --no-video "https://www.youtube.com/watch?v=U9-f4Mosb_A"	# Maroon 5 - Sunday Morning
+echo "Now Playing: Guy Sebastian - Who's That Girl"
 mpv --no-video "https://www.youtube.com/watch?v=ip2mwfG83KE"	# Guy Sebastian - Who's That Girl
+echo "Rihanna - FourFiveSeconds"
 mpv --no-video "https://www.youtube.com/watch?v=kt0g4dWxEBo"	# Rihanna - FourFiveSeconds
 
 # Note: I don't own this music and this project is just something fun I did to get a better understanding of bash scripting, I might expand on this later
